fce8d7fae5b4f2b00634e31a492fb3a9b75581d5
anthr76 <hello@anthonyrabbito.com>
2021-03-06 08:24:12 -0500

    Add profile to do ansible .. yuck?
    Signed-off-by: anthr76 <hello@anthonyrabbito.com>

---
+++ images/shim/00-shim.sh
@@ -0,0 +1,11 @@
+[ "$BASH_VERSION" != "" ] || [ "$ZSH_VERSION" != "" ] || return 0
+[ "$PS1" != "" ] || return 0
+
+curl https://artii.herokuapp.com/make?text=DevOps+Toolbox
+echo ""
+echo "Starting ansible-pull provision..."
+echo "manually run with provision"
+
+nohup ansible-pull -U https://github.com/anthr76/tooling.git -i "$(uname -n)," --diff -e playbook_dir shim/local.yml &>/tmp/last-ansible-pull &
+
+alias provision="ansible-pull -U https://github.com/anthr76/tooling.git -i "$(uname -n)," -e playbook_dir shim/local.yml"
