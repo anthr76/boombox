84dab75a2d58748645dc1e450044d63261f5008c
Ryan Walter <rwalt@pm.me>
2021-10-08 17:50:01 -0500

    feat: switched Chezmoi, sops, and go-task to dnf
    Signed-off-by: Ryan Walter <rwalt@pm.me>

--- hack/github-releases.sh
+++ hack/github-releases.sh
@@ -10,14 +10,6 @@ chmod +x /usr/local/bin/calicoctl
 test -x /usr/local/bin/calicoctl 2>&1 || exit 1
 rm -rf /tmp/*
 
-# renovate: datasource=github-releases depName=twpayne/chezmoi
-export CHEZMOI_VERSION=v2.6.1
-curl -fsSL "https://github.com/twpayne/chezmoi/releases/download/${CHEZMOI_VERSION}/chezmoi_${CHEZMOI_VERSION#*v}_linux_amd64.tar.gz" \
-    | tar xvz -f - -C /tmp
-mv /tmp/chezmoi /usr/local/bin/chezmoi
-test -x /usr/local/bin/chezmoi 2>&1 || exit 1
-rm -rf /tmp/*
-
 # renovate: datasource=github-releases depName=schollz/croc
 export CROC_VERSION=v9.4.2
 curl -fsSL "https://github.com/schollz/croc/releases/download/${CROC_VERSION}/croc_${CROC_VERSION#*v}_Linux-64bit.tar.gz" \
@@ -32,14 +24,6 @@ curl -fsSL -o /usr/local/bin/kubie \
     "https://github.com/sbstp/kubie/releases/download/${KUBIE_VERSION}/kubie-linux-amd64"
 chmod +x /usr/local/bin/kubie
 test -x /usr/local/bin/kubie 2>&1 || exit 1
-rm -rf /tmp/*
-
-# renovate: datasource=github-releases depName=mozilla/sops
-export SOPS_VERSION=v3.7.1
-curl -fsSL -o /usr/local/bin/sops \
-    "https://github.com/mozilla/sops/releases/download/${SOPS_VERSION}/sops-${SOPS_VERSION}.linux"
-chmod +x /usr/local/bin/sops
-test -x /usr/local/bin/sops 2>&1 || exit 1
 rm -rf /tmp/*
 
 # renovate: datasource=github-releases depName=mvdan/sh
@@ -64,14 +48,6 @@ curl -fsSL "https://github.com/stern/stern/releases/download/${STERN_VERSION}/st
     | tar xvz -f - --strip-components=1 -C /tmp
 mv /tmp/stern /usr/local/bin/stern
 test -x /usr/local/bin/stern 2>&1 || exit 1
-rm -rf /tmp/*
-
-# renovate: datasource=github-releases depName=go-task/task
-export TASK_VERSION=v3.9.0
-curl -fsSL "https://github.com/go-task/task/releases/download/${TASK_VERSION}/task_linux_amd64.tar.gz" \
-    | tar xvz -f - -C /tmp
-mv /tmp/task /usr/local/bin/task
-test -x /usr/local/bin/task 2>&1 || exit 1
 rm -rf /tmp/*
 
 # renovate: datasource=github-releases depName=vmware-tanzu/velero
